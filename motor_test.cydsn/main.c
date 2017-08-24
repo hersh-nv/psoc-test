e12053cc40570e04146cbf0b5f49baf319bbf94c
hersh-nv <h.nevgi@gmail.com>
2017-08-24 16:48:20 +1000

    Add new project for basic motor control, and shaft encoder input later

---
+++ motor_test.cydsn/main.c
@@ -0,0 +1,48 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* DRIVE IN TWO DIRECTIONS */
+    /* ======================= */
+    
+    //enable
+    H_EN_Write(1);
+    
+    // direction A
+    A1_Write(1);
+    A2_Write(0);
+    CyDelay(5000);
+    
+    // stop
+    A1_Write(0);
+    A2_Write(0);
+    CyDelay(1000);
+    
+    // direction B
+    A1_Write(0);
+    A2_Write(1);
+    CyDelay(5000);
+    
+    // stop
+    A1_Write(0);
+    A2_Write(0);
+    for(;;)
+    {
+
+    }
+}
+
+/* [] END OF FILE */
