13e780c72c244ccf4ffca120bba8258c197ae39a
hersh-nv <h.nevgi@gmail.com>
2017-08-24 17:21:38 +1000

    Make motor speed soft-coded

--- motor_test.cydsn/main.c
+++ motor_test.cydsn/main.c
@@ -15,7 +15,7 @@ int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
-    /* DRIVE IN TWO DIRECTIONS */
+    /* DRIVE AT VARIOUS SPEEDS */
     /* ======================= */
     
     PWM_1_Start();
@@ -23,24 +23,30 @@ int main(void)
     // enable
     PWM_1_Enable();
     
+    // 25% speed
+    PWM_1_WriteCompare(63);
+    
     // direction A
     A1_Write(1);
     A2_Write(0);
-    CyDelay(5000);
+    CyDelay(2000);
+        
+    // 50% speed
+    PWM_1_WriteCompare(127);
+    CyDelay(2000);
+    
+    // 75% speed
+    PWM_1_WriteCompare(191);
+    CyDelay(2000);
+    
+    // 100% speed
+    PWM_1_WriteCompare(255);
+    CyDelay(3000);
     
     // stop
     A1_Write(0);
     A2_Write(0);
-    CyDelay(1000);
     
-    // direction B
-    A1_Write(0);
-    A2_Write(1);
-    CyDelay(5000);
-    
-    // stop
-    A1_Write(0);
-    A2_Write(0);
     for(;;)
     {
 
